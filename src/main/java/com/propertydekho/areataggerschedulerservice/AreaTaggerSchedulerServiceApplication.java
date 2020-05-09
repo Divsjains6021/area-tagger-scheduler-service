99802580252354a5b91e441f1b8cdb81865c01e7
Divsjains6021 <divsjains6021@gmail.com>
2020-05-09 23:36:06 +0530

    Property area tagger scheduler service repo

---
+++ src/main/java/com/propertydekho/areataggerschedulerservice/AreaTaggerSchedulerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.propertydekho.areataggerschedulerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AreaTaggerSchedulerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AreaTaggerSchedulerServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/propertydekho/areataggerschedulerservice/AreaTaggerSchedulerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.propertydekho.areataggerschedulerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AreaTaggerSchedulerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
